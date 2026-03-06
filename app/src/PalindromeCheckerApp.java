bb8bf45079300cd6108204da161c100b06a83b4b
dipesh-kumar-pirojiwala <codmdipeshgamer@gmail.com>
2026-03-06 09:26:32 +0530

    UC6 completed

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -26,7 +26,7 @@ public class PalindromeCheckerApp{
         if (isPalindrome) {
             System.out.println(input + " is a Palindrome.");
         } else {
-            System.out.println(input + " is NOT a Palindrome.");
+            System.out.println(input + " is not a Palindrome.");
         }
     }
 }
