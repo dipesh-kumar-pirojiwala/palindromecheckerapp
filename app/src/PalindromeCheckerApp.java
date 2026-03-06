fdb82fe98e836b3a3d291c9621d4cccb2ec0866e
dipesh-kumar-pirojiwala <codmdipeshgamer@gmail.com>
2026-03-06 10:10:47 +0530

    UC13 completed

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,12 +1,54 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
-public class PalindromeCheckerApp{
-    public static boolean isPalindrome(String str) {
+public class PalindromeCheckerApp {
+
+    public static boolean stackMethod(String input) {
+
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < normalized.length(); i++) {
+            stack.push(normalized.charAt(i));
+        }
+
+        for (int i = 0; i < normalized.length(); i++) {
+            if (normalized.charAt(i) != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean dequeMethod(String input) {
+
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < normalized.length(); i++) {
+            deque.addLast(normalized.charAt(i));
+        }
+
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean twoPointerMethod(String input) {
+
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
         int start = 0;
-        int end = str.length() - 1;
+        int end = normalized.length() - 1;
 
         while (start < end) {
-            if (str.charAt(start) != str.charAt(end)) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
                 return false;
             }
             start++;
@@ -15,23 +57,33 @@ public class PalindromeCheckerApp{
 
         return true;
     }
-    public static void main(String args[]){
+
+    public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
-        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+        long startTime1 = System.nanoTime();
+        boolean result1 = stackMethod(input);
+        long endTime1 = System.nanoTime();
 
-        boolean result = isPalindrome(normalized);
+        long startTime2 = System.nanoTime();
+        boolean result2 = dequeMethod(input);
+        long endTime2 = System.nanoTime();
 
-        if (result) {
-            System.out.println("The given string is a Palindrome (Ignoring spaces and case).");
-        } else {
-            System.out.println("The given string is NOT a Palindrome (Ignoring spaces and case).");
-        }
+        long startTime3 = System.nanoTime();
+        boolean result3 = twoPointerMethod(input);
+        long endTime3 = System.nanoTime();
+
+        System.out.println("\nResults:");
+        System.out.println("Stack Method: " + result1 + " | Time: " + (endTime1 - startTime1) + " ns");
+        System.out.println("Deque Method: " + result2 + " | Time: " + (endTime2 - startTime2) + " ns");
+        System.out.println("Two Pointer Method: " + result3 + " | Time: " + (endTime3 - startTime3) + " ns");
 
         scanner.close();
     }
 }
+
 
